7c281f7ec2dc8aa1c5ba54f826cdac6463ade1cd
rgowtham25 <rgowtham2502@gmail.com>
2025-06-23 01:00:10 +0530

    added twilio account sid and autho token

--- notifier/whatsapp_sender.py
+++ notifier/whatsapp_sender.py
@@ -4,8 +4,10 @@ from twilio.rest import Client
 import os
 
 # 🔒 Set your credentials (or load from environment variables)
-TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
-TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+# TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+# TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+TWILIO_ACCOUNT_SID = "ACe85de827b0eba12fd4f2ca1a211ef98c"
+TWILIO_AUTH_TOKEN = "2bc4e8a6ddf9f0918d4031e9ae9fbefb"
 TWILIO_WHATSAPP_NUMBER = "whatsapp:+14155238886"  # Twilio Sandbox number
 RECIPIENT_NUMBER = "whatsapp:+919750257792"       # Your number (must be verified in sandbox)
 
